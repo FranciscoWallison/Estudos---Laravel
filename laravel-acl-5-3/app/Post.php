176f5eb3d8b143cffe32690c51298118d356d6e1
Francisco WAllison <franciscowallison@gmail.com>
2017-02-20 10:51:45 -0300

    Eloquent: Mutators
    Defining An Accessor

--- laravel-acl-5-3/app/Post.php
+++ laravel-acl-5-3/app/Post.php
@@ -6,8 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+   
     //
+    protected $fillable = ['user_id','title','description'];
+    
     public function user(){
     	return $this->belongsTo(User::class);
-    } 
+    }
+    
+    public function getFirstCreated_atAttribute($value)
+    {
+        return \Carbon::parse($value)->format('d/m/Y H:i:s');
+    }
+    
 }
