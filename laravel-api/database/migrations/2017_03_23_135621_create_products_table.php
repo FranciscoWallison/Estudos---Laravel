09720b92460d5f4c0dffff041c45b2d41cda6830
vagrant <vagrant@homestead>
2017-03-23 14:00:19 +0000

    Aula 03 - Migrations

--- laravel-api/database/migrations/2017_03_23_135621_create_products_table.php
+++ laravel-api/database/migrations/2017_03_23_135621_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',100);
-            $table->string('email',100)->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name',100)->unique();
+            $table->text('description',1000);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
