3bcda5340454deee7054015dab00333c456dfd26
Manoel / Wallison <programador2@pc4.com.br>
2017-03-31 09:52:13 -0300

    Aula 06 - Listando todos os Produtos www.laravel.com/docs/master/controllers#resource-controllers

--- laravel-api/app/Http/Controllers/API/ProductController.php
+++ laravel-api/app/Http/Controllers/API/ProductController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
+    private $product;
+    public  function  __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +21,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return response()->json(['success' => 'Index']);
+        $products = $this->product->all();
+
+        return response()->json(['data' => $products]);
     }
 
     /**
